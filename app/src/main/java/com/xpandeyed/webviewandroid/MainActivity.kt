3798fe0318c77e944e9aa276e806f251ef6b476a
lalbiharipandeyg@gmail.com <lalbiharipandeyg@gmail.com>
2021-10-06 10:27:44 +0530

    web view with google forms completed.

---
+++ app/src/main/java/com/xpandeyed/webviewandroid/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.xpandeyed.webviewandroid
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.bToWebView).setOnClickListener {
+            val intent = Intent(this, WebActivity::class.java)
+            startActivity(intent)
+        }
+
+    }
+}
---
+++ app/src/main/java/com/xpandeyed/webviewandroid/WebActivity.kt
@@ -0,0 +1,20 @@
+package com.xpandeyed.webviewandroid
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
+
+
+
+
+class WebActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_web_actiivity)
+        val wbMyWebView = findViewById<WebView>(R.id.wbMyWebView)
+        wbMyWebView.webViewClient = WebViewClient()
+        wbMyWebView.getSettings().setJavaScriptEnabled(true)
+        wbMyWebView.loadUrl("https://docs.google.com/forms/d/e/1FAIpQLSfMmIbJaABCCrUleQn5eftLnyPZ9N9AMU96MvnDUdMzDJww7A/viewform?usp=sf_link")
+    }
+}
